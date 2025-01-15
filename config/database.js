8f20ef97bb5887ddcfc851ce596132724a8a65e4
akmal abid <akmal.abid53@gmail.com>
2025-01-15 17:33:12 +0700

    change db config

--- config/database.js
+++ config/database.js
@@ -1,23 +1,7 @@
 // //mysql 1 for producution 
-// const {createPool} = require("mysql")
+const {createPool} = require("mysql")
 
-// const pool = createPool ({
-//     port:process.env.DB_PORT,
-//     host:process.env.DB_HOST,
-//     user:process.env.DB_USER,
-//     password:process.env.DB_PASSWORD,
-//     database:process.env.MYSQL_DB
-// })
-
-// module.exports = pool
-
-
-//mysql 2 for local development
-
-const mysql = require('mysql2');
-
-
-const pool = mysql.createConnection ({
+const pool = createPool ({
     port:process.env.DB_PORT,
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
@@ -25,5 +9,21 @@ const pool = mysql.createConnection ({
     database:process.env.MYSQL_DB
 })
 
-
 module.exports = pool
+
+
+//mysql 2 for local development
+
+// const mysql = require('mysql2');
+
+
+// const pool = mysql.createConnection ({
+//     port:process.env.DB_PORT,
+//     host:process.env.DB_HOST,
+//     user:process.env.DB_USER,
+//     password:process.env.DB_PASSWORD,
+//     database:process.env.MYSQL_DB
+// })
+
+
+// module.exports = pool
